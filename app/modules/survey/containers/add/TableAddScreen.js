9c3585a7f4a409ae50887f2077ad0aee1a1e9534
Erik Ilyin <erik.ilyin@aol.com>
2018-01-26 06:25:08 +0300

    survey add screen

--- app/modules/survey/containers/add/TableAddScreen.js
+++ app/modules/survey/containers/add/TableAddScreen.js
@@ -1,111 +1,11 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container, Header, Title, Content, Button, Item, Label, Input, Body, Left, Right, Icon, Form, Text, Segment } from 'native-base';
+import SurveyAddScreen from './SurveyAddScreen'
 
-import { Actions } from 'react-native-router-flux';
-import SurveyAddForm from '../../components/form/SurveyAddForm';
-import {addSurvey, updateSurvey} from '../../actions'
-import { addAct, updateAct } from '../../../../actions/api';
 
-const surveyInitial = {
-  questions:[],
-  answers:[],
-  frequency: '1d',
-}
-
-class SurveyTableAddScreen extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: 1,
-    };
-  }
-
-  pushRoute(route) {
-    Actions.push(route)
-  }
-
-  popRoute() {
-    Actions.pop()
-  }
-
-  onAddSurvey = (body) => {    
-    const {addSurvey, addAct} = this.props
-    let data = {...body, questions: [], mode: 'table'}
-    let params = { act_data: data, type:'survey', title}
-    return addAct(params).then( res => {
-      Actions.push('survey-edit-question',{actIndex:0, questionIndex:0})
-    }).catch(err => {
-      console.log(err)
-      Toast.show({text: 'Error! '+err.message, type: 'danger', buttonText: 'OK' })
-    })
-    
-  }
-
-  onEditSurvey = (body) => {
-    let {actIndex, acts, user, updateAct} = this.props
-    let survey = {...this.state.survey, ...body}
-    let {title, ...act_data} = survey
-    let act = {id:acts[actIndex].id, title, act_data}
-
-    if(user.role == 'clinician') {
-      return updateAct(actIndex, act).then(result => {
-        Actions.pop()
-      }).catch(err => {
-        Toast.show({text: 'Error! '+err.message, type: 'danger', buttonText: 'OK' })
-      })
-    } else {
-      Actions.pop()
-    }
-  }
-
-  componentWillMount() {
-    let {surveys, surveyIdx} = this.props
-    if(surveyIdx) {
-      const survey = surveys[surveyIdx]
-      this.setState({survey})
-    } else {
-      this.setState({})
-    }
-  }
-
-  render() {
-    const {survey} = this.state;
-    const {actIndex, acts} = this.props
-    let title = survey ? survey.title : "New Table Survey"
-    return (
-      <Container>
-        <Header hasTabs>
-          <Left>
-            <Button transparent onPress={() => Actions.pop()}>
-              <Icon name="arrow-back" />
-            </Button>
-          </Left>
-          <Body style={{flex:3}}>
-            <Title>{title}</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Content padder>
-            {actIndex!=undefined ? (<SurveyAddForm onSubmit={this.onEditSurvey} initialValues={survey} act={acts[actIndex]}/>) : (<SurveyAddForm onSubmit={this.onAddSurvey} initialValues={surveyInitial}/>) }
-          </Content>
-        </Content>
-      </Container>
-    );
+export default class TableAddScreen extends SurveyAddScreen {
+  getMode() {
+    return 'table'
   }
 }
-
-const mapDispatchToProps = {
-  addAct, updateAct
-}
-
-const mapStateToProps = state => ({
-  acts: state.core.acts,
-  themeState: state.drawer.themeState,
-  user: state.core.auth,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SurveyTableAddScreen);
